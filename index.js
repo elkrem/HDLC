e1e7876f4e04ee2c626a4e308d3c74dc5f8ae952
SaW- <e.saw.90@gmail.com>
2019-01-30 15:54:04 +0200

    build

--- index.js
+++ index.js
@@ -1,37 +1,38 @@
-const crc = require("crc");
-const EventEmitter = require("events").EventEmitter;
-const FRAME_BOUNDARY_OCTET = 0x7E;
-const CONTROL_ESCAPE_OCTET = 0x7D;
-const INVERT_OCTET = 0x20;
-const MINIHDLC_MAX_FRAME_LENGTH = 4096;
-
-class HDLC {
-    public pendingFrame: any = {};
-    public eventEmitter = new EventEmitter();
-    public byteSendingFunction = (byte) => {};
-
-    public init(byteSendingFunction) {
+"use strict";
+exports.__esModule = true;
+var crc = require("crc");
+var EventEmitter = require("events").EventEmitter;
+var FRAME_BOUNDARY_OCTET = 0x7E;
+var CONTROL_ESCAPE_OCTET = 0x7D;
+var INVERT_OCTET = 0x20;
+var MINIHDLC_MAX_FRAME_LENGTH = 4096;
+var HDLC = /** @class */ (function () {
+    function HDLC() {
+        this.pendingFrame = {};
+        this.eventEmitter = new EventEmitter();
+        this.byteSendingFunction = function (byte) { };
+    }
+    HDLC.prototype.init = function (byteSendingFunction) {
         this.byteSendingFunction = byteSendingFunction;
         this.pendingFrame.framePosition = 0;
         this.pendingFrame.frameChecksum = undefined;
         this.pendingFrame.escapeCharacter = false;
         this.pendingFrame.receivedFrameBuffer = [];
         this.pendingFrame.isStared = false;
-    }
-
-    public sendchar(data) {
+    };
+    HDLC.prototype.sendchar = function (data) {
         this.byteSendingFunction(data & 0xff);
-    }
-
-    public byteReceiver(bytes) {
-        for (let i = 0; i < bytes.length; i++) {
+    };
+    HDLC.prototype.byteReceiver = function (bytes) {
+        for (var i = 0; i < bytes.length; i++) {
             /* FRAME FLAG */
-            let data = bytes[i];
+            var data = bytes[i];
             /* FRAME FLAG */
             if (data === FRAME_BOUNDARY_OCTET) {
                 if (this.pendingFrame.escapeCharacter === true) {
                     this.pendingFrame.escapeCharacter = false;
-                } else if ((this.pendingFrame.framePosition >= 2) &&
+                }
+                else if ((this.pendingFrame.framePosition >= 2) &&
                     (this.pendingFrame.frameChecksum === ((this.pendingFrame.receivedFrameBuffer[this.pendingFrame.framePosition - 1] << 8) | (this.pendingFrame.receivedFrameBuffer[this.pendingFrame.framePosition - 2] & 0xff)))) {
                     /* Call the user defined function and pass frame to it */
                     this.eventEmitter.emit("newFrame", this.pendingFrame.receivedFrameBuffer.slice(0, this.pendingFrame.receivedFrameBuffer.length - 2));
@@ -42,23 +43,19 @@ class HDLC {
                 this.pendingFrame.receivedFrameBuffer = [];
                 continue;
             }
-
             if (this.pendingFrame.escapeCharacter) {
                 this.pendingFrame.escapeCharacter = false;
                 data ^= INVERT_OCTET;
-            } else if (data === CONTROL_ESCAPE_OCTET) {
+            }
+            else if (data === CONTROL_ESCAPE_OCTET) {
                 this.pendingFrame.escapeCharacter = true;
                 continue;
             }
-
             this.pendingFrame.receivedFrameBuffer[this.pendingFrame.framePosition] = data;
-
             if (this.pendingFrame.framePosition - 2 >= 0) {
                 this.pendingFrame.frameChecksum = crc.crc16ccitt([this.pendingFrame.receivedFrameBuffer[this.pendingFrame.framePosition - 2]], this.pendingFrame.frameChecksum);
             }
-
             this.pendingFrame.framePosition++;
-
             if (this.pendingFrame.framePosition === MINIHDLC_MAX_FRAME_LENGTH) {
                 this.pendingFrame.framePosition = 0;
                 this.pendingFrame.frameChecksum = undefined;
@@ -66,15 +63,12 @@ class HDLC {
                 this.pendingFrame.receivedFrameBuffer = [];
             }
         }
-    }
-
-    public sendFrame(rawFrame) {
-        let byte;
-        let fcs;
-
+    };
+    HDLC.prototype.sendFrame = function (rawFrame) {
+        var byte;
+        var fcs;
         this.sendchar(FRAME_BOUNDARY_OCTET);
-
-        for (let i = 0; i < rawFrame.length; i++) {
+        for (var i = 0; i < rawFrame.length; i++) {
             byte = rawFrame[i];
             fcs = crc.crc16ccitt([byte], fcs);
             if ((byte === CONTROL_ESCAPE_OCTET) || (byte === FRAME_BOUNDARY_OCTET)) {
@@ -83,7 +77,6 @@ class HDLC {
             }
             this.sendchar(byte);
         }
-
         byte = fcs;
         if ((byte === CONTROL_ESCAPE_OCTET) || (byte === FRAME_BOUNDARY_OCTET)) {
             this.sendchar(CONTROL_ESCAPE_OCTET);
@@ -97,7 +90,7 @@ class HDLC {
         }
         this.sendchar(byte);
         this.sendchar(FRAME_BOUNDARY_OCTET);
-    }
-}
-
-export {HDLC};
+    };
+    return HDLC;
+}());
+exports.HDLC = HDLC;
